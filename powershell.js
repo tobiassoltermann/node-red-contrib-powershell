1f1518321879b2ff7aca4b7c9835ea0a47c4d28a
Tot Ara <clertem@gmail.com>
2023-12-10 04:03:42 +1300

    Spawns pwshe.exe only when message is received.

--- powershell.js
+++ powershell.js
@@ -5,32 +5,30 @@ const shell = require('node-powershell');
 module.exports = function(RED) {
     function PowershellNode(config) {
         RED.nodes.createNode(this, config);
-        let ps = new shell({
-            executionPolicy: 'Bypass',
-            noProfile: true,
-            pwsh: true
-        });
-
+        
         this.on('input', function(msg) {
-            ps.addCommand(msg.payload);
+            let ps = new shell({
+				executionPolicy: 'Bypass',
+				noProfile: true,
+				pwsh: true
+			});
+			
+			ps.addCommand(msg.payload);
+			
             ps.invoke()
-            .then(output => {
-                msg.payload = output;
-                this.send(msg);
-            })
-            .catch(error => {
-                msg.payload = error;
-                this.error(error, msg);
-            });
+				.then(output => {
+					msg.payload = output;
+					this.send(msg);
+					ps.dispose();
+				})
+				.catch(error => {
+					msg.payload = error;
+					this.error(error, msg);
+					ps.dispose();
+				});
+				
         });
-
-        this.on('close', function() {
-
-            ps.dispose();
-        })
     }
-
-
 
     RED.nodes.registerType("powershell", PowershellNode);
 }
